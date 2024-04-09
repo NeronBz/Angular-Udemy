9582172f3ef888af290c539bc4576adbaf3f1ad0
Raul.vn <rblazquezi02@educarex.es>
2024-04-09 16:51:11 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule, DbzModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10,
+    },
+  ];
+
+  @Output()
+  public onDeleteId: EventEmitter<string> = new EventEmitter();
+
+  deleteCharacterById(id: string): void {
+    console.log({ id });
+    this.onDeleteId.emit(id);
+  }
+}
